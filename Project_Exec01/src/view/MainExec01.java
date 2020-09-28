8488b0ef3859d929028dce2615af974b98599ac2
Fernando Oliveira <fernansoft111@gmail.com>
2020-09-28 18:39:52 -0300

    Exec01 - Servidor
    Modificando o contador de ID das threads na main de 0 a 20 para 1 a 21, para corresponder ao enunciado.

--- Project_Exec01/src/view/MainExec01.java
+++ Project_Exec01/src/view/MainExec01.java
@@ -36,7 +36,7 @@ public class MainExec01 {
     public static void main(String[] args) {
         int permissoes = 1;
         Semaphore semaforo = new Semaphore(permissoes);
-        for (int i = 0; i < 21; i++) {
+        for (int i = 1; i < 22; i++) {
             Thread operacao = new ThreadOperacoes(i, semaforo);
             operacao.start();
         }
